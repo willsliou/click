30d23df11fef7577ca9d1ec40572c35f03012e64
Wills <willsliou@protonmail.com>
2024-10-30 22:56:51 -0700

    init

---
+++ assets/chunk-d598e92d.js
@@ -0,0 +1,1 @@
+const t={configName:"Default",accountSettings:{accountEmail:"",accountPassword:""},autofillSettings:{saveApplications:!0,autoClickNextPage:!1,autoSubmit:!1}};export{t as I};
---
+++ service-worker-loader.js
@@ -0,0 +1,1 @@
+import './assets/chunk-a8dd98cc.js';
